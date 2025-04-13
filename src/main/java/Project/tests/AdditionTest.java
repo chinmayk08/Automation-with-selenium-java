4fdf9741a5679530cd6b51387574f38e04290de3
chinmay kanekar <chinmaykanekar2@gmail.com>
2025-04-13 21:25:58 +0530

    13042025

--- src/main/java/Project/tests/AdditionTest.java
+++ src/main/java/Project/tests/AdditionTest.java
@@ -2,8 +2,12 @@ package Project.tests;
 
 import Project.businessLib.ArithmaticOperations;
 import Project.framework.ExecutionDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 public class AdditionTest extends ExecutionDriver {
 
@@ -21,6 +25,8 @@ public class AdditionTest extends ExecutionDriver {
     }
     @Test(priority = 3)
     public void ispositiveagativeTest() {
+        WebDriverWait wt=new WebDriverWait(driver, Duration.ofSeconds(10));
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.MINUTES);
         ArithmaticOperations add = new ArithmaticOperations(driver);
         int actualresult = add.performOperation("Add", 50, 80, -70, -10);
         Assert.assertEquals(actualresult, 50);
