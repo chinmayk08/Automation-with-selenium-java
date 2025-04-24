5cfb6feebe9887db8599ee6102d3ced78084c888
chinmay kanekar <chinmaykanekar2@gmail.com>
2025-04-24 22:49:04 +0530

    24/04/2025

--- src/main/java/corejava/testng/DataProviderString.java
+++ src/main/java/corejava/testng/DataProviderString.java
@@ -6,17 +6,20 @@ import org.testng.annotations.Test;
 public class DataProviderString {
 
     @Test(dataProvider = "browser type")
-    public void testmethod(String param1,String param2){
-        System.out.println(param1+" "+param2);
+    public void testmethod(String param1,String param2, String pp){
+        System.out.println(param1+" "+param2+" "+pp);
     }
 
     @DataProvider(name = "browser type")
     public Object[][] data(){
-        System.out.println("in data provider");
+        //System.out.println("in data provider");
         return new Object[][]
                 {
-                        {"abc","xyz"},
-                        {"pwe","dff"}
+                        {"abc","xyz","ooo"},
+                        {"pwe","dff","iuy"}
                 };
     }
 }
+
+
+
