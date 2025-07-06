0b2b4fbe62c33573f6f0f3ab9142dd12327fcb0b
chinmay kanekar <chinmaykanekar2@gmail.com>
2025-07-06 23:00:12 +0530

    06/07/2025

--- src/main/java/JavaPracticeHome/HighestNumber.java
+++ src/main/java/JavaPracticeHome/HighestNumber.java
@@ -6,12 +6,19 @@ public class HighestNumber {
 
         int[] a={10,50,20,30};
         int highest=0;
+        int smallest=a[0];
 
         for (int i=0;i<a.length;i++){
             if(a[i]>highest){
                 highest=a[i];
             }
         }
+        for (int i=0;i<a.length;i++){
+            if(a[i]<smallest){
+                smallest=a[i];
+            }
+        }
         System.out.println(highest);
+        System.out.println(smallest);
     }
 }
